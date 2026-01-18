5fa72adc2bb2a59319c5c8a066c1f398953c0b1b
Gennady Uraltsev <genandy.uraltsev@gmail.com>
2026-01-18 17:49:27 -0600

    Info components

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .prelude import *
 from .NamedFunction import NamedFunction as NamedFunction
-from .numpify import numpify as numpify
+from .numpify import numpify as numpify, numpify_cached 
 from .SmartFigure import SmartFigure as Figure
 # from .SmartException import *
 # from .SmartFigure import *
